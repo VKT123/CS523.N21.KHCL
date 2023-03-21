c7888e7a106a481c787a3e43c9e0537f35269a52
Jun0se7en <93256752+Jun0se7en@users.noreply.github.com>
2023-03-21 16:00:37 +0700

    Add files via upload

---
+++ Source Code/Java/Brute_Force.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Brute_Force {
+    public Brute_Force()
+    {}
+    public int search(String pat, String txt) 
+    {
+        int M = pat.length();
+        int N = txt.length();
+        for (int i = 0; i <= N - M; i++)
+        { 
+            int j;
+            for (j = 0; j < M; j++)
+                if (txt.charAt(i+j) != pat.charAt(j))
+                    break;
+            if (j == M) return i; // found
+        }
+        return N; // not found 
+    }
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a text: ");
+        String txt = scanner.nextLine();
+        System.out.print("Enter a pattern: ");
+        String pat = scanner.nextLine();
+        scanner.close();
+        Brute_Force BF = new Brute_Force();
+        int offset = BF.search(pat,txt);
+        if (offset==txt.length())
+            System.out.println("Pattern not found");
+        else
+            System.out.println(pat+" start at " + offset);
+
+    }
+}
